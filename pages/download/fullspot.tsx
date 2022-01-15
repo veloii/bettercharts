f39c6d264b7709570224cb693496ce1895eff7ac
ZelrDev <zelr.apps@gmail.com>
2022-01-15 16:32:10 +0000

    add download screen

--- pages/download/fullspot.tsx
+++ pages/download/fullspot.tsx
@@ -10,7 +10,18 @@ const fullspot = () => {
     );
   }, []);
 
-  return <div>Downloading file</div>;
+  return (
+    <div className="h-screen w-screen waves">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white">
+        <div className="text-3xl font-mono text-center font-semibold">
+          Downloading file
+        </div>
+        <div className="text-lg font-mono text-center">
+          fullspot-1.0.0-setup.exe
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default fullspot;
